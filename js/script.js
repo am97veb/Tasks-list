f414113e4aef8800e1dd84cc05b63f390bf05592
Adrian Markefka <am97veb@gmail.com>
2024-05-05 15:59:08 +0200

    remove tasks

--- js/script.js
+++ js/script.js
@@ -8,29 +8,43 @@
         },
     ];
 
+    const addNewTask = (newTaskContent) => {
+        tasks.push({
+            content: newTaskContent,
+        });
+
+        render();
+    };
+
+    const removeTask = (taskIndex) => {
+        tasks.splice(taskIndex,1);
+        render();
+    };
+
     const render = () => {
         let htmlString = "";
 
         for (const task of tasks) {
             htmlString += `
                 <li>
+                <button class="js-remove">usuń zadanie</button>
                     ${task.content}
                 </li>
             `;
         }
 
         document.querySelector(".js-tasks").innerHTML = htmlString;
+
+        const removeButtons = document.querySelectorAll(".js-remove");
+
+        removeButtons.forEach ((removeButton, index) => {
+            removeButton.addEventListener("click", () => {
+                removeTask(index);
+            })
+        })
     };
 
-   const addNewTask = (newTaskContent) => {
-    tasks.push({
-        content: newTaskContent,
-    });
-    
-    render();
-   };
-
-   const onFormSubmit = (event) => {
+    const onFormSubmit = (event) => {
         event.preventDefault();
 
         const newTaskContent = document.querySelector(".js-newTask").value.trim();
@@ -38,7 +52,7 @@
         if (newTaskContent === "") {
             return;
         }
-        
+
         addNewTask(newTaskContent);
 
     };
