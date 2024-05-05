eb7c4318a52eb3ef82960252f5ea760f0cd083ce
Adrian Markefka <am97veb@gmail.com>
2024-05-05 14:29:01 +0200

    initial commit

---
+++ js/script.js
@@ -0,0 +1,30 @@
+{
+    const tasks = [
+        {
+            content: "zjeść śniadanie",
+        },
+        {
+            content: "zrobić zadanie domowe",
+        },
+    ];
+
+    const render = () => {
+        let htmlString = "";
+
+        for (const task of tasks) {
+            htmlString += `
+                <li>
+                    ${task.content}
+                </li>
+            `;
+        }
+
+        document.querySelector(".js-tasks").innerHTML = htmlString;
+    };
+
+    const init = () => {
+        render();
+    }
+
+    init();
+};
