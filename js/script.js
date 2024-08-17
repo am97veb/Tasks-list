a8ee034d701e8b310fbcfd2baaedaf2e9f1227fd
Adrian Markefka <am97veb@gmail.com>
2024-08-17 16:15:40 +0200

    new button function add

--- js/script.js
+++ js/script.js
@@ -5,7 +5,6 @@
 
     let hideDoneTask = false;
 
-
     const addNewTask = (newTaskContent) => {
         tasks = [
             ...tasks,
@@ -41,6 +40,15 @@
         render();
     };
 
+    const toggleHideDoneTask = () => {
+        tasks.forEach((tasks, taskIndex) => {
+            if (tasks[taskIndex], done = true) {
+                hideDoneTask = !hideDoneTask;
+            };
+            render();
+        });
+    };
+
     const bindEvents = () => {
         const removeButtons = document.querySelectorAll(".js-remove");
 
@@ -59,19 +67,21 @@
         });
     };
 
-    const renderButtons = (taskIndex) => {
+    const renderButtons = () => {
         const newButtons = document.querySelector(".js-newButtons");
 
-        if (tasks.length !== 0) {
-            newButtons.innerHTML = `
-                <li>
-                    <button class="js-showDoneTasks">ukryj ukończone zadania</button>
-                    <button class="js-doneAllTasks" >ukończ wszystkie</button>
-                </li>
+        for (task of tasks) {
+            if (tasks.length !== 0) {
+                newButtons.innerHTML = `
+                <div class="newButtons">
+                    <button class="newButton js-showDoneTasks ">${hideDoneTask ? "Pokaż" : "Ukryj"} ukończone </button>
+                    <button class="newButton js-doneAllTasks" ${task.done ? "disabled" : ""}>Ukończ wszystkie</button>
+                </div>
             `;
-            return;
+                return;
+            };
+            newButtons.innerHTML = "";
         };
-        newButtons.innerHTML = "";
     };
 
     const renderTasks = () => {
@@ -79,7 +89,7 @@
 
         for (const task of tasks) {
             htmlString += `
-                <li class="list__item">
+                <li class="list__item ${hideDoneTask ? "hideItem" : ""}">
                     <button class="listButton js-done">${task.done ? "✔" : ""}</button>
                     <p class="listText${task.done ? " list__item--done" : ""}">${task.content}<p>
                     <button class="listButton--remove js-remove">🗑</button>
@@ -90,28 +100,32 @@
         document.querySelector(".js-tasks").innerHTML = htmlString;
     };
 
-    const bindButtonsEvents = (taskIndex) => {
-
+    const bindButtonsEvents = () => {
         const showDoneTasks = document.querySelector(".js-showDoneTasks");
 
+        if (showDoneTasks !== null) {
+            tasks.forEach((tasks, taskIndex) => {
+                showDoneTasks.addEventListener("click", () => {
+                    toggleHideDoneTask(taskIndex);
+                });
+            });
+        };
 
         const doneAllTasks = document.querySelector(".js-doneAllTasks");
-
 
         if (doneAllTasks !== null) {
             tasks.forEach((tasks, taskIndex) => {
                 doneAllTasks.addEventListener("click", () => {
-                toggleTaskDoneAll(taskIndex);
+                    toggleTaskDoneAll(taskIndex);
                 });
             });
-            return;
         };
     };
 
     const render = () => {
         renderTasks();
         renderButtons();
-    
+
         bindEvents();
         bindButtonsEvents();
     };
@@ -129,7 +143,6 @@
         }
 
         newTaskElement.focus();
-
     };
 
     const init = () => {
